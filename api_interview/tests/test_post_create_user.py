9482ad0de23389354737dcfd705021222c72a438
bartlomiejfydrych <fydbar@gmail.com>
2022-10-20 18:05:57 +0200

    [API INTERVIEW] Add post_user admin true test

--- api_interview/tests/test_post_create_user.py
+++ api_interview/tests/test_post_create_user.py
@@ -1,3 +1,54 @@
+import json
+
+from jsonschema.validators import validate
+
+from api_interview.requests.post_create_user import post_create_user
+from api_interview.tests_data.data_post_create_user import schema_post_create_user, CreateUserDTO
+from utils.tests_info import show_tests
+
+
+def test_post_create_user_true_admin():
+    response = post_create_user(
+        username="Marcin",
+        age=30,
+        admin=True,
+        skills=["Chodzenie", "Skakanie", "Pływanie"],
+        city="Wąchock",
+        street="Oświęcimska",
+        street_number="5a",
+        additional=[{"key": "Ulubione jedzenie", "value": "Kotlety"}]
+    )
+    resp = response.json()
+
+    # Pobranie ID usera
+    user_id = resp["id"]
+
+    # ----------------------
+    # Basic response tests:
+    # ----------------------
+    test_a = "Response should have status code 200"
+    assert response.status_code == 200
+
+    test_b = "Response should have correct Schema"
+    validate(resp, schema_post_create_user)
+
+    test_c = "Response should have correct Data Transfer Object (DTO)"
+    CreateUserDTO.validate(resp)
+
+    # ----------------------
+    # Detailed tests:
+    # ----------------------
+    test_1 = "Add user should have correct data"
+    response_body = json.loads(response.request.body.decode('uft-8'))
+    del resp["id"]
+    assert resp == response_body
+
+    # ----------------------
+    # Wyświetlenie testów:
+    # ----------------------
+    show_tests(test_a, test_b, test_c, test_1)
+
+
 '''
 PLAN TESTÓW:
 Utworzenie użytkownika z jedną umiejętnością i jednym obiektem "additional" oraz admin true.
---
+++ api_interview/tests_data/data_post_create_user.py
@@ -0,0 +1,72 @@
+from pydantic import BaseModel, StrictStr
+
+schema_post_create_user = {
+    "type": "object",
+    "properties": {
+        "id": {"type": "integer"},
+        "username": {"type": "string"},
+        "age": {"type": "integer"},
+        "admin": {"type": "boolean"},
+        "skills": {
+            "type": "array",
+            "items": {"type": "string"}
+        },
+        "location": {
+            "type": "object",
+            "properties": {
+                "city": {"type": "string"},
+                "street": {"type": "string"},
+                "street_number": {"type": "string"}
+            },
+            "required": [
+                "city",
+                "street",
+                "street_number"
+            ]
+        },
+        "additional": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "key": {"type": "string"},
+                    "value": {"type": "string"}
+                },
+                "required": [
+                    "key",
+                    "value"
+                ]
+            }
+        }
+    },
+    "required": [
+        "id",
+        "username",
+        "age",
+        "admin",
+        "skills",
+        "location",
+        "additional"
+    ]
+}
+
+
+class Location(BaseModel):
+    city: StrictStr
+    street: StrictStr
+    street_number: StrictStr
+
+
+class AdditionalItem(BaseModel):
+    key: StrictStr
+    value: StrictStr
+
+
+class CreateUserDTO(BaseModel):
+    id: int
+    username: StrictStr
+    age: int
+    admin: bool
+    skills: list[StrictStr]
+    location: Location
+    additional: list[AdditionalItem]
