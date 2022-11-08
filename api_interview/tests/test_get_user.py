57adec14c52082e375ded92d90a14fa165075122
bartlomiejfydrych <fydbar@gmail.com>
2022-11-08 19:05:16 +0100

    [API INTERVIEW] Add get no exist user test

---
+++ api_interview/tests/test_get_user.py
@@ -0,0 +1,24 @@
+from api_interview.requests.get_user import get_user
+from utils.response_show import show_response_data
+from utils.tests_info import show_tests
+
+
+def test_get_user_no_exist_id():
+    response = get_user(99999)
+    resp = response.json()
+
+    # ----------------------
+    # Basic response tests:
+    # ----------------------
+    test_a = "Response should have status code 404"
+    assert response.status_code == 404
+
+    # ----------------------
+    # Detailed tests:
+    # ----------------------
+    test_1 = "Response should have correct error message"
+    assert resp["status"] == 404
+    assert resp["message"] == "Could not find user with id '99999'"
+
+    # Wyświetlanie testów:
+    show_tests(test_a, test_1)
