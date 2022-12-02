007886231f145acfae4558f53c4eabbc25e51ca2
bartlomiejfydrych <fydbar@gmail.com>
2022-12-02 17:50:28 +0100

    [API INTERVIEW] Add more GET LIST of user tests

--- api_interview/tests/test_get_user_list.py
+++ api_interview/tests/test_get_user_list.py
@@ -1,4 +1,7 @@
+from api_interview.requests.delete_user import delete_user
 from api_interview.requests.get_user_list import get_user_list
+from api_interview.requests.post_create_user import post_create_user
+from api_interview.requests.put_edit_user import put_edit_user
 from utils.response_show import show_response_data
 
 
@@ -21,8 +24,72 @@ def test_get_user_list_add_user(create_delete_user):
     assert create_delete_user == added_user
 
 
-# def test_get_user_list_edit_user(create_delete_user):
+def test_get_user_list_edit_user(create_delete_user):
+    response_put = put_edit_user(
+        create_delete_user["id"],
+        username="Radim",
+        age=66,
+        admin=True,
+        skills=["Chodzenie", "Skakanie", "Pływanie"],
+        city="Wąchock",
+        street="Oświęcimska",
+        street_number="5a",
+        additional=[{"key": "Ulubione jedzenie", "value": ""}]
+    )
+    resp_put = response_put.json()
+
+    response = get_user_list()
+    show_response_data(response)
+    resp = response.json()
+
+    # ----------------------
+    # Basic response tests:
+    # ----------------------
+    test_a = "Response should have status code 200"
+    assert response.status_code == 200
+
+    # ----------------------
+    # Detailed tests:
+    # ----------------------
+    test_1 = "Response should have correct data"
+    edit_user = next((user for user in resp if user["id"] == create_delete_user["id"]), None)
+    assert resp_put == edit_user
 
 
+def test_get_user_list_delete_user():
+    # Add user
+    response_post = post_create_user(
+        username="Edward",
+        age=44,
+        admin=True,
+        skills=["Chodzenie", "Pływanie"],
+        city="Radom",
+        street="Sokolska",
+        street_number="9g",
+        additional=[{"key": "Język", "value": "Angielski"}]
+    )
+    resp_post = response_post.json()
+    user_id = resp_post["id"]
+    assert response_post.status_code == 201
 
-# def test_get_user_list_delete_user():
+    # Delete user
+    response_delete = delete_user(user_id)
+    assert response_delete.status_code == 204
+
+    # Get user
+    response = get_user_list()
+    show_response_data(response)
+    resp = response.json()
+
+    # ----------------------
+    # Basic response tests:
+    # ----------------------
+    test_a = "Response should have status code 200"
+    assert response.status_code == 200
+
+    # ----------------------
+    # Detailed tests:
+    # ----------------------
+    test_1 = "Response should have correct data"
+    deleted_user = next((user for user in resp if user["id"] == user_id), None)
+    assert deleted_user is None
